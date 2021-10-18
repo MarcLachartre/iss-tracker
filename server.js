495132655d83d9eea2cd884d61f54c7bfd36b5d4
marclachartre <marc.lachartre@gmail.com>
2021-10-18 21:51:06 +0200

    modified dependencies

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(
 );
 
 
-// Serve the files on port 3000.
-app.listen(3000, function () {
-  console.log('App listening on port 3000!\n');
+// Serve the files on port || 3000.
+app.listen(process.env.PORT || 3000, function () {
+  console.log('App listening on port || 3000!\n');
 });
