1b5e05233e50dd047190a09ec89a8c7f55008787
marclachartre <marc.lachartre@gmail.com>
2021-10-18 15:47:07 +0200

    modified rules loader

--- webpack.common.js
+++ webpack.common.js
@@ -15,7 +15,9 @@ module.exports = {
       title: 'ISS Tracker',
       template: './src/html/index.html'
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin({
+        filename: "index.css"
+    })
   ],
 
   output: {
